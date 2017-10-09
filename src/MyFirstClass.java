16350e94ac3799f08f102aba59433a0343db9947
Travis-Gray2194 <travisogray1@gmail.com>
2017-10-09 11:05:37 -0400

    Initial commit

---
+++ src/MyFirstClass.java
@@ -0,0 +1,8 @@
+public class MyFirstClass {//Start class name
+
+    public static void main (String[] args) {//Main Method
+
+        System.out.println("Hello World!");
+
+    }//End main method
+}//End Class name
